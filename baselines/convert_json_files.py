d04e85b8bad0a9c3279d74b16f563014394777a3
NayanaBannur <42090597+NayanaBannur@users.noreply.github.com>
2022-03-18 17:56:25 +0000

    Update to create caption-only, table-only datasets

--- baselines/convert_json_files.py
+++ baselines/convert_json_files.py
@@ -7,11 +7,16 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("-f", "--file", help="The SciGen json file to be converted for pretrained models' input format", required=True)
     parser.add_argument("-s", "--split", help="Specify the corresponding split, i.e., train, dev, or test", required=True)
-
+    parser.add_argument("-c", "--caption-only", help="Create dataset from only captions", type=bool, default=False, required=False)
+    parser.add_argument("-t", "--table_only", help="Create dataset from only table", type=bool, default=False, required=False)
+    parser.add_argument("-r", "--remove_tags", help="Remove tags", type=bool, default=False, required=False)
 
     args = parser.parse_args()
     file = args.file
     out = args.split
+    caption_only = args.caption_only
+    table_only = args.table_only
+    remove_tags = args.remove_tags
 
     row_seperator = '<R>'
     cell_separator = '<C>'
@@ -22,21 +27,32 @@ if __name__ == '__main__':
             with open('{}.target'.format(out), 'w', encoding='utf-8') as target:
                 data = json.load(f)
                 for d in data:
-                    text = row_seperator + ' ' + cell_separator
-                    row_len = len(data[d]['table_column_names'])
-                    for i,c in enumerate(data[d]['table_column_names']):
-                        text += ' ' + c
-                        if i < row_len-1: 
-                            text += ' ' + cell_separator
-
-                    for row in data[d]['table_content_values']:
-                        text += ' ' + row_seperator + ' ' + cell_separator
-                        for i, c in enumerate(row):
+                    if not args.caption_only:
+                        text = row_seperator + ' ' + cell_separator
+                        row_len = len(data[d]['table_column_names'])
+                        for i,c in enumerate(data[d]['table_column_names']):
                             text += ' ' + c
-                            if i < row_len -1:
+                            if i < row_len-1: 
                                 text += ' ' + cell_separator
 
-                    text += ' ' + caption_separator + ' ' +data[d]['table_caption'] + '\n'
+                        for row in data[d]['table_content_values']:
+                            text += ' ' + row_seperator + ' ' + cell_separator
+                            for i, c in enumerate(row):
+                                text += ' ' + c
+                                if i < row_len -1:
+                                    text += ' ' + cell_separator
+
+                        if not args.table_only:    
+                            text += ' ' + caption_separator + ' ' +data[d]['table_caption'] + '\n'
+                        else:
+                            text += '\n'
+
+                    else:
+                        text = data[d]['table_caption'] + '\n'
+                    
+                    if args.remove_tags:
+                        for tag in ['[BOLD]', '[EMPTY]', '[CONTINUE]', '[ITALIC]', '<bold>', '</bold>', '<italic>', '</italic>']:
+                            text = text.replace(tag, '')
 
                     source.write(text)
                     
