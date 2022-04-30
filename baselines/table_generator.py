cd893009221e7973176a9948d83257b0e99b1082
NayanaBannur <42090597+NayanaBannur@users.noreply.github.com>
2022-04-30 23:03:42 +0000

    Updated table generator

--- baselines/table_generator.py
+++ baselines/table_generator.py
@@ -5,7 +5,9 @@ train_size = 200
 dev_size = 10
 train_papers = {}
 dev_papers = {}
-num_cols = 6 
+num_cols = 6
+num_rows = 4
+row_names = 'ABCD'
 
 for i in range (train_size+dev_size):
     paper = {}
@@ -13,38 +15,57 @@ for i in range (train_size+dev_size):
     paper["paper_id"] = ""
     paper["table_caption"]  = "Table"
     paper["table_column_names"] = ["Col"+str(i) for i in range (num_cols)]
-    difference = np.random.randint(0, 10)
-    A = np.random.randint(0, 10, num_cols)
-    '''
-    if (i % 2 == 0):
-        B = np.random.random(num_cols) * 10 + 10
-        C = np.random.random(num_cols) * 10 + 20
-    if (i % 2 == 1):
-        B = np.random.random(num_cols) * 10 + 20
-        C = np.random.random(num_cols) * 10 + 10
-    '''
-    B = np.random.randint(10, 20, num_cols)
-    C = B + difference
 
-    A = ['A'] + [str(round(A[i], 4)) for i in range (num_cols - 1)]
-    B = ['B'] + [str(round(B[i], 4)) for i in range (num_cols - 1)] 
-    C = ['C'] + [str(round(C[i], 4)) for i in range (num_cols - 1)]
+    rows = []
+    highest_cols = []
+    for j in range (num_rows):
+        perm = np.random.permutation(num_cols - 1)
+        highest_cols.append("Col" + str(np.argmax(perm)+1))
+        rows.append([row_names[j]] + [str(perm[k]) for k in range (len(perm))])
 
-    paper["table_content_values"] = [A, B, C]
-    '''
-    if (i % 2 == 0):
-        paper["text"] = "C is larger than B"
-    else:
-        paper["text"] = "B is larger than C"
-    '''
-    paper["text"] = "C is " + str(difference) + " larger than B"
+    paper["table_content_values"] = rows
+    paper["text"] = ", ".join(highest_cols)
     if i < train_size:
         train_papers[str(i)] = paper
     else:
         dev_papers[str(i-train_size)] = paper
 
+smallest = False
+if smallest:
+    for i in range (train_size+dev_size, 2*(train_size+dev_size)):
+        paper = {}
+        paper["paper"] = ""
+        paper["paper_id"] = ""
+        paper["table_caption"]  = "Table"
+        paper["table_column_names"] = ["Col"+str(i) for i in range (num_cols)]
+
+        rows = []
+        for j in range(num_rows):
+            rows.append(np.random.randint(100, 150, num_cols))
+
+        rows[i%num_rows] = [x-np.random.randint(100, 150) for x in rows[i%num_rows]]
+
+        for j in range(num_rows):
+            rows[j] = [row_names[j]] + [str(rows[j][k]) for k in range (num_cols - 1)]
+
+        paper["table_content_values"] = rows
+        paper["text"] = f"{row_names[i%num_rows]} is the smallest."
+        if i < 2*train_size+dev_size:
+            train_papers[str(i)] = paper
+        else:
+            dev_papers[str(i-train_size)] = paper
+
+
+with open('../data/train/few-shot/train.json', 'w') as fp:
+    json.dump(train_papers, fp, indent = 4)
+
+with open('../data/dev/few-shot/dev.json', 'w') as fp:
+    json.dump(dev_papers, fp, indent = 4)
+'''
 with open('train.json', 'w') as fp:
     json.dump(train_papers, fp, indent = 4)
 
 with open('dev.json', 'w') as fp:
     json.dump(dev_papers, fp, indent = 4)
+'''
+
